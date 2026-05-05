75cf99b59b33ca679de58d53db5ad78e9fcefc14
Cyber-Ella <emmanuellaobi05@gmail.com>
2026-05-05 12:44:15 +0100

    Update mailer configuration to use SMTP settings

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
+  requireTLS: true,
 });
 
 module.exports = transporter;
