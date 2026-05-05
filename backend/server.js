c0b5769d18e6f8ed3a0c080fcd8c7e7d440fa40a
Cyber-Ella <emmanuellaobi05@gmail.com>
2026-05-05 12:18:14 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const SubscriberRouter = require("./routes/subscribeRoutes");
 const startMorningJob = require("./jobs/morningJob");
+const sendEmail = require("./service/emailService")
 const app = express();
 
 app.use(express.json());
@@ -16,6 +17,21 @@ startMorningJob();
 
 app.get("/", (req, res) => {
   res.send("Morning email bot running");
+});
+
+app.get("/test-email", async (req, res) => {
+  try {
+    await sendEmail(
+      "cyberella001@gmail.com",
+      "SMTP Test",
+      "If you see this, email works"
+    );
+
+    res.send("Email sent successfully");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Email failed: " + err.message);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
