b655e78c4cf31dc9bbbd4da9be3565ac845754ff
Cyber-Ella <emmanuellaobi05@gmail.com>
2026-05-06 07:31:55 +0100

    Fix email service import path in subscribeRoutes

--- backend/routes/subscribeRoutes.js
+++ backend/routes/subscribeRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const sendEmail= require("service.emailService")
+const sendEmail= require("../service.emailService")
 const Subscriber = require("../model/Subscriber");
 
 router.post("/subscribe", async (req, res) => {
