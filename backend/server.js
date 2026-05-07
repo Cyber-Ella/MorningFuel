bb8a107a08a47f56eb00713feb064034c839feed
Cyber-Ella <emmanuellaobi05@gmail.com>
2026-05-07 07:53:40 +0100

    Refactor email sending and remove morning job

--- backend/server.js
+++ backend/server.js
@@ -6,34 +6,38 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const SubscriberRouter = require("./routes/subscribeRoutes");
-const startMorningJob = require("./jobs/morningJob");
 const sendEmail = require("./service/emailService")
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-dotenv.config();
-connectDB();
-app.use("/api", SubscriberRouter);
 
-startMorningJob();
+connectDB();
+
+app.use("/api", SubscriberRouter);
 
 app.get("/", (req, res) => {
   res.send("Morning email bot is running");
 });
 
 app.get("/test-email", async (req, res) => {
+  if (req.query.key !== process.env.CRON_KEY) {
+    return res.status(403).send("Unauthorized");
+  }
   try {
     await sendEmail(
-      "emmanuellaobi05@gmail.com",
+      "cyberella001@gmail.com",
       "SMTP Test",
       "If you see this, email works"
     );
-
     res.send("Email sent successfully");
   } catch (err) {
     console.log(err);
-    res.status(500).send("Email failed: " + err.message);
+    res.status(500).send(
+      "Email failed: " + err.message
+    );
   }
 });
 
